db333dbccc5471be84e9293b8aba5a340ad94b95
Mike McCabe <mikemccabe85@gmail.com>
2021-08-08 08:38:22 +0100

    basic apache setup

--- windows-lb/ubuntu-setup-dotnet-apache.sh
+++ windows-lb/ubuntu-setup-dotnet-apache.sh
@@ -8,22 +8,27 @@ sudo snap install --channel=5.0/edge dotnet-sdk --classic
 sudo snap alias dotnet-sdk.dotnet dotnet
 export DOTNET_ROOT=/snap/dotnet-sdk/current
 
+sudo apt-get update
 sudo apt-get install apache2 -y
-sudo a2enmod proxy proxy_http proxy_html proxy_wstunnel
-sudo a2enmod rewrite
 
-sudo cp userdata/apache/netcore.conf /etc/apache2/conf-enabled/
+# sudo a2enmod proxy proxy_http proxy_html proxy_wstunnel
+# sudo a2enmod rewrite
+# sudo cp userdata/apache/netcore.conf /etc/apache2/conf-enabled/
 
 sudo service apache2 restart
 sudo apachectl configtest
 
-sudo cp -a ~/userdata/testgit /var/netcore/
+# This is from the Oracle page to allow http
+sudo iptables -I INPUT 6 -m state --state NEW -p tcp --dport 80 -j ACCEPT
+sudo netfilter-persistent save
 
-sudo cp ~/userdata/apache/ServiceFile.service /etc/systemd/system/
+# # sudo cp -a ~/userdata/testgit /var/netcore/
 
-sudo systemctl enable ServiceFile.service
-sudo systemctl stop ServiceFile.service
-sudo systemctl start ServiceFile.service
+# sudo cp ~/userdata/apache/ServiceFile.service /etc/systemd/system/
+
+# sudo systemctl enable ServiceFile.service
+# sudo systemctl stop ServiceFile.service
+# sudo systemctl start ServiceFile.service
 
 # systemctl restart apache2
 exit
